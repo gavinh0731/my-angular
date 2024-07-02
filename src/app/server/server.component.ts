b4e7b408b0600412e484d6e515a5500a2df99f47
gavin <gavin@hisharp.com.tw>
2024-07-02 08:24:31 +0000

    first commit

---
+++ src/app/server/server.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-server',
+  templateUrl: './server.component.html',
+  styleUrl: './server.component.scss'
+})
+export class ServerComponent {
+  serverId: number = 10;
+  serverStatus: string = "離線";
+}
