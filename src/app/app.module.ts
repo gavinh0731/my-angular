496abfcd7a1b1e8937e8ad2cfd24ad3c15c50550
gavin <gavin@hisharp.com.tw>
2024-07-04 01:13:46 +0000

    組件

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,12 +6,14 @@ import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 import { ServerComponent } from './server/server.component';
 import { ServersComponent } from './servers/servers.component';
+import { TestPipe } from './pipes/test.pipe';
 
 @NgModule({
   declarations: [
     AppComponent,
     ServerComponent,
-    ServersComponent
+    ServersComponent,
+    TestPipe
   ],
   imports: [
     BrowserModule,
--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormControl, FormGroup } from '@angular/forms';
+import { FormControl, FormGroup, FormBuilder, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-servers',
@@ -25,7 +25,21 @@ export class ServersComponent implements OnInit {
     password: ''
   };
   invalid = "form-red-text";
-  // -----------------------
+  //錯誤提醒資料
+  formErrors = {
+    'title': '',
+    'content': ''
+  };
+  valiDataForm = this.fb.group({
+    userName: ['',
+      [Validators.required,
+      Validators.maxLength(18),
+      Validators.minLength(6)]],
+    password: ['', [this.passWordVal]],
+    phone: ['', [Validators.required, this.phoneVal],]
+  });
+  dateStr: Date = new Date();
+  // ---------------------------------------------------------------------------
   allowNewServer = false;
   imgUrl = "data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAACAAAAAgCAAAAABWESUoAAABhUlEQVR4AczOA6xcARCF4X/us23btuLUQR3WZlhbQW3bto2gtm3b2J3GN4tY74uHh3okOi42Nj4AAUDwTvDEhisABCOAEAzYTBh0bN+lc5d0QgAhHPqX4Y4pkd367Zc+xMcXCPDg8TPwxeQH724Uj9N95GJ4pnHxby1xgCmV3v+GsFb7k0Iee7UjaQgmIYe9v9O4+wUKWaATyMPAViwvr1NnOQWDdTV5CHYSaKWLmaGDyvU4mTgQcllracaFr+9uk+yLM8k8fAJff0IUTghRVFu3M1ynkYk4nchiqqU3Ry05RDibMMhjtj6Ht3dJxVk/l1G64cspOusychBsCWm01WNM0jFs0sbEYUtIIuTXNco588OLh09JxE6wweuXEAvfL9PQus3+iU80N34KCaQyUOcxTTuSjEmMRI5rS9JwIZfD1t7ceo34YIpnuQ4nGwP8XXlq/fJZd9jE8KBHC5IwAELJ7dWrZ6fOuNhkgFgAEEIAwA1MQkw0AgDC/wnKycrKSQ2iPAkAVyNzq8rxicIAAAAASUVORK5CYII=";
 
@@ -34,7 +48,7 @@ export class ServersComponent implements OnInit {
   serverCreated = false;
   servers = ["ServerTest", "ServerTest1", "ServerTest2"]
 
-  constructor() {
+  constructor(private fb: FormBuilder) {
     // 按鈕在 數秒之後就變成不可按
     setTimeout(() => {
       this.allowNewServer = true;
@@ -66,8 +80,24 @@ export class ServersComponent implements OnInit {
     console.log(this.loginForm.value["userName"]);
   }
 
-  subBtnFUn(obj: any) {
+  subBtnFun(obj: any) {
     console.log(obj)
+  }
+
+  phoneVal(phone: FormControl): object {
+    const value = phone.value || '';
+    if (!value) return { desc: '請輸入手機號' }
+    const valid = /[0-9]{11}/.test(value);
+    return valid ? {} : { desc: '聯絡電話必須是11位數字' }
+  }
+  passWordVal(password: FormControl): object {
+    const value = password.value || '';
+    const valid = value.match(/^(?![0-9]+$)(?![a-zA-Z]+$)[0-9A-Za-z]{6,20}$/);
+    return valid ? {} : { passwordValidator: { desc: '密碼至少包含 數字和英文，長度6-20' } }
+  }
+
+  subValiDataFormFun() {
+    console.log(this.valiDataForm.get("userName"));
   }
   // ---------------------------------------------------------------------------
   onCreateServer() {
