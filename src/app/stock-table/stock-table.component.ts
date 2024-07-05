459153dc356b20fda79e7065aa90124717c59477
gavin <gavin@hisharp.com.tw>
2024-07-05 07:29:34 +0000

    視角

--- src/app/stock-table/stock-table.component.ts
+++ src/app/stock-table/stock-table.component.ts
@@ -3,9 +3,18 @@ import { Component, AfterViewInit, ViewChild } from '@angular/core';
 import { MatPaginator, MatPaginatorModule } from '@angular/material/paginator';
 import { MatTableDataSource, MatTableModule } from '@angular/material/table';
 import { MatSort, Sort, MatSortModule } from '@angular/material/sort';
+import { MatSelectModule } from '@angular/material/select';
+import { MatFormFieldModule } from '@angular/material/form-field';
 // standalone
 import { CommonModule } from '@angular/common';  // 確保導入 CommonModule
 
+// === DropDown Menu ===
+interface Food {
+  value: string;
+  viewValue: string;
+}
+
+// === ELEMENT_DATA ===
 export interface PeriodicElement {
   name: string;
   position: number;
@@ -31,7 +40,7 @@ const ELEMENT_DATA: PeriodicElement[] = [
   templateUrl: './stock-table.component.html',
   styleUrl: './stock-table.component.scss',
   standalone: true,
-  imports: [MatTableModule, MatPaginatorModule, MatSortModule, CommonModule],
+  imports: [MatTableModule, MatPaginatorModule, MatSortModule, CommonModule, MatFormFieldModule, MatSelectModule],
 })
 export class StockTableComponent implements AfterViewInit {
   displayedColumns: string[] = ['position', 'name', 'weight', 'symbol'];
@@ -42,10 +51,34 @@ export class StockTableComponent implements AfterViewInit {
   @ViewChild(MatPaginator) paginator: MatPaginator;
   @ViewChild(MatSort) sort: MatSort;
 
+  // region === === 生命週期 === === === === === === === === === === === === ===
+  ngOnChanges() {
+    console.log("1. ngOnChanges");
+  }
+  ngOnInit() {
+    console.log("2. ngOnInit");
+  }
+  // ngDoCheck() {
+  //   console.log("3. ngDoCheck");
+  // }
+  // ngAfterContentInit() {
+  //   console.log("4. ngAfterContentInit");
+  // }
+  // ngAfterContentChecked() {
+  //   console.log("5. ngAfterContentChecked");
+  // }
   ngAfterViewInit() {
+    console.log("6. ngAfterViewInit");
     this.dataSource.paginator = this.paginator;
     this.dataSource.sort = this.sort;
   }
+  // ngAfterViewChecked() {
+  //   console.log("7. ngAfterViewChecked");
+  // }
+  ngOnDestroy() {
+    console.log("8. ngOnDestroy");
+  }
+  // region --- --- 生命週期 --- --- --- --- --- --- --- --- --- --- --- --- ---
 
   /** Announce the change in sort state for assistive technology. */
   announceSortChange(sortState: Sort) {
@@ -60,6 +93,44 @@ export class StockTableComponent implements AfterViewInit {
     }
   }
 
+
+
+  // CSS
+  isHighlighted(value: number): boolean {
+    return value > 2; // 條件設定
+  }
+
+  // === DropDown Menu ===
+  selected: string;
+  foods: Food[] = [
+    { value: 'position', viewValue: 'Position' },
+    { value: 'name', viewValue: 'Name' },
+    { value: 'weight', viewValue: 'Weight' },
+    { value: 'symbol', viewValue: 'Symbol' },
+  ];
+
+  changeDisplayedColumns(perspective: any) {
+    console.log(`perspective = ${perspective}`);
+    switch (perspective) {
+      case 'position': {
+        this.displayedColumns = ['position'];
+        break;
+      }
+      case 'name': {
+        this.displayedColumns = ['name'];
+        break;
+      }
+      case 'weight': {
+        this.displayedColumns = ['weight'];
+        break;
+      }
+      default: {
+        this.displayedColumns = ['symbol'];
+        break;
+      }
+    }
+  }
+
   toggleColumn(column: string) {
     const index = this.displayedColumns.indexOf(column);
     if (index >= 0) {
@@ -67,10 +138,5 @@ export class StockTableComponent implements AfterViewInit {
     } else {
       this.displayedColumns.push(column);
     }
-  }
-
-  // CSS
-  isHighlighted(value: number): boolean {
-    return value > 2; // 條件設定
   }
 }
